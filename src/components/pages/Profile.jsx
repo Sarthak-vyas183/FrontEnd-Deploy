db9151b7a320be809a11f86cd341b84a0e17eead
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-08-29 23:10:21 +0530

    Req to admin to become a doctor : feature implemented

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,25 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useAuth } from "../Store/useAuth";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 function Profile() {
   const { user, token } = useAuth();
+
   const navigate = useNavigate();
   const [profileImg, setProfileImg] = useState();
-  const [defaultProfileImg, setDefaultProfileImg] = useState("../../../images/user.png"); 
+  const [defaultProfileImg, setDefaultProfileImg] = useState(
+    "../../../images/user.png"
+  );
+
+  const [DoctorForm, setDoctorForm] = useState(false);
+  const [RMP_NO, setRMP_NO] = useState("");
+  const [RMP_img, setRMP_img] = useState(null);
+
+  const [deletePassword, setDeletePassword] = useState("");
+  const [isDeleting, setIsDeleting] = useState(false);
 
   const [isEditing, setIsEditing] = useState(false);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [deletePassword, setDeletePassword] = useState("");
-
-  const [fullname, setFullname] = useState(user?.fullname || '');
-  const [email, setEmail] = useState(user?.email || '');
-  const [location, setLocation] = useState(user?.location || '');
-  const [bio, setBio] = useState(user?.bio || '');
-  const [ph_no, setPhNo] = useState(user?.ph_no || '');
-  const [preferredLanguage, setLanguage] = useState(user?.preferredLanguage || '');
+  const [fullname, setFullname] = useState(user?.fullname || "");
+  const [email, setEmail] = useState(user?.email || "");
+  const [location, setLocation] = useState(user?.location || "");
+  const [bio, setBio] = useState(user?.bio || "");
+  const [ph_no, setPhNo] = useState(user?.ph_no || "");
+  const [preferredLanguage, setLanguage] = useState(
+    user?.preferredLanguage || ""
+  );
   const [profileImageFile, setProfileImageFile] = useState(null);
 
   useEffect(() => {
@@ -44,16 +53,22 @@ function Profile() {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
-        body: JSON.stringify({ fullname, email, location, bio, ph_no, preferredLanguage }),
+        body: JSON.stringify({
+          fullname,
+          email,
+          location,
+          bio,
+          ph_no,
+          preferredLanguage,
+        }),
       });
 
       if (!response.ok) {
-        throw new Error('Internal Server Error');
+        throw new Error("Internal Server Error");
       }
 
       alert("Profile Updated");
       setIsEditing(false);
-      navigate("/");
     } catch (error) {
       alert("Error: Unable to save profile. Try again later.");
       console.error(error);
@@ -77,32 +92,32 @@ function Profile() {
       alert("Please Upload Profile Pic");
       return;
     }
-  
+
     const formData = new FormData();
-    formData.append('profileImage', profileImageFile);
-  
+    formData.append("profileImage", profileImageFile);
+
     try {
       const response = await fetch(`${baseUrl}/user/updateProfile`, {
-        method: 'POST',
+        method: "POST",
         headers: {
           Authorization: `Bearer ${token}`,
         },
-        body: formData, 
+        body: formData,
       });
-  
+
       if (!response.ok) {
-        throw new Error('Internal Server Error');
+        throw new Error("Internal Server Error");
       }
-  
+
       const result = await response.json();
-  
+
       if (result.profileimg) {
         setProfileImg(result.profileimg);
       }
-  
+
       alert("Profile Image Updated");
-      navigate("/");
-      setProfileImageFile(null); 
+
+      setProfileImageFile(null);
     } catch (error) {
       alert("Error: Unable to update profile. Try again later.");
       console.error(error);
@@ -118,10 +133,49 @@ function Profile() {
     setDeletePassword("");
   };
 
+  const handleBecomeDoctorCancelclick = () => {
+    setDoctorForm(false);
+    setRMP_NO("");
+    setRMP_img("");
+  };
+
+  const handleReqToBeDoctor = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("RMP_No", RMP_NO);
+      formData.append("RMP_img", RMP_img);
+  
+      const response = await fetch(`${baseUrl}/auth/become_doctor`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: formData,
+      });
+  
+      if (!response.ok) {
+        const text = await response.text();
+        console.log(text);
+        return alert("Failed to send request: " + text);
+      }
+  
+      const data = await response.json(); 
+      alert("Your request to become a doctor has been sent to the admin");
+      handleBecomeDoctorCancelclick();
+    } catch (error) {
+      console.log(`Error occurred: ${error}`);
+      alert("An error occurred while sending the request.");
+    }
+  };
+  
+  const BecomeDoctor = () => {
+    setDoctorForm(true);
+  };
+
   const handleDeleteAccount = async () => {
     try {
-      const response = await fetch(`${baseUrl}/user/delete_account`, {
-        method: 'POST',
+      const response = await fetch(`${baseUrl}/auth/delete_account`, {
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
@@ -130,14 +184,15 @@ function Profile() {
       });
 
       if (!response.ok) {
-        throw new Error('Failed to delete account.');
+        throw new Error("Failed to delete account.");
       }
-
+      localStorage.removeItem("token");
       alert("Account deleted successfully.");
-      navigate("/")
-      // Implement redirect or further actions after account deletion
+      navigate("/");
     } catch (error) {
-      alert("Error: Unable to delete account. Please check your password and try again.");
+      alert(
+        "Error: Unable to delete account. Please check your password and try again."
+      );
       console.error(error);
     }
   };
@@ -157,10 +212,19 @@ function Profile() {
       <div className="bg-white rounded-lg shadow-md p-6 flex flex-col md:flex-row items-center justify-center md:justify-between mb-8">
         <div className="flex flex-col items-center gap-4 mb-4 md:mb-0 md:flex-row md:items-center md:gap-6">
           <div className="flex flex-col items-center">
-            <img className="w-20 h-20 md:w-24 md:h-24 rounded-full" src={getImageSrc(profileImg) || defaultProfileImg} alt="Profile" />
+            <img
+              className="w-20 h-20 md:w-24 md:h-24 rounded-full"
+              src={getImageSrc(profileImg) || defaultProfileImg}
+              alt="Profile"
+            />
             <p className="font-semibold text-blue-600 underline cursor-pointer flex flex-col items-center mt-2">
               <span onClick={UpdateProfile}>Update</span>
-              <input className="text-[10px]" type="file" name='profilePic' onChange={handleFileChange}/>
+              <input
+                className="text-[10px]"
+                type="file"
+                name="profilePic"
+                onChange={handleFileChange}
+              />
             </p>
           </div>
 
@@ -174,7 +238,12 @@ function Profile() {
           <p className="text-gray-600 text-center">{user?.email}</p>
           <div className="text-center">
             <h2 className="text-sm text-gray-500">Current time</h2>
-            <p>{new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
+            <p>
+              {new Date().toLocaleTimeString([], {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+            </p>
           </div>
         </div>
       </div>
@@ -183,25 +252,34 @@ function Profile() {
         <div className="w-full md:w-[60%]">
           <div className="bg-white rounded-lg shadow-md p-6 mb-6">
             <h2 className="text-lg font-semibold mb-4">Bio</h2>
-            <p className="text-gray-600">{user?.bio || "Lorem ipsum dolor sit amet, consectetur adipisicing elit."}</p>
+            <p className="text-gray-600">
+              {user?.bio ||
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit."}
+            </p>
           </div>
           <div className="bg-white rounded-lg shadow-md p-6 mb-6">
             <h2 className="text-lg font-semibold mb-4">My Activity</h2>
             <div className="flex justify-between">
               <p>Created Remedy</p>
-              <span className="bg-green-100 my-2 text-green-700 px-2 py-1 rounded">{user?.remedyList.length || "0"}</span>
+              <span className="bg-green-100 my-2 text-green-700 px-2 py-1 rounded">
+                {user?.remedyList.length || "0"}
+              </span>
             </div>
             <div className="flex justify-between">
               <p>Badge</p>
-              <span className="bg-gray-200 my-2 text-gray-500 px-2 py-1 rounded">{user?.badge || "Not yet"}</span>
+              <span className="bg-gray-200 my-2 text-gray-500 px-2 py-1 rounded">
+                {user?.badge || "Not yet"}
+              </span>
             </div>
             <div className="flex justify-between">
               <p>Certificate</p>
-              <span className="bg-gray-200 my-2 text-gray-500 px-2 py-1 rounded">{user?.badge || "Not yet"}</span>
+              <span className="bg-gray-200 my-2 text-gray-500 px-2 py-1 rounded">
+                {user?.badge || "Not yet"}
+              </span>
             </div>
           </div>
         </div>
-  
+
         <div className="w-full md:w-[40%]">
           <div className="bg-white rounded-lg shadow-md p-6 mb-6">
             <h2 className="text-lg font-semibold mb-4">Details</h2>
@@ -219,36 +297,64 @@ function Profile() {
             </div>
           </div>
           <div className="bg-white rounded-lg shadow-md p-6">
-            <h2 className="text-lg font-semibold mb-4">Controls</h2> 
+            <h2 className="text-lg font-semibold mb-4">Controls</h2>
 
-            {user.isDoctor ? (
+            <div
+              className="flex justify-between mt-4 mb-4"
+              onClick={() => alert("This panel is in Development")}
+            >
+              <p>Get Certificate</p>
+              <p className="bg-blue-100 px-2 py-1 text-blue-700 rounded cursor-pointer">
+                Get Now
+              </p>
+            </div>
+
+            <div
+              className="flex justify-between mt-4 mb-4"
+              onClick={() => alert("This panel is in Development")}
+            >
+              <p>Reset Password</p>
+              <p className="bg-green-100 px-2 py-1 text-green-700 rounded cursor-pointer">
+                Reset
+              </p>
+            </div>
+
+            <div
+              className="flex justify-between mt-4 mb-4"
+              onClick={handleEditClick}
+            >
+              <p>Edit Profile</p>
+              <p className="bg-yellow-100 px-2 py-1 text-yellow-700 rounded cursor-pointer">
+                Edit
+              </p>
+            </div>
+
+            <div
+              className="flex justify-between mt-4 mb-4"
+              onClick={handleDeleteClick}
+            >
+              <p>Delete Account</p>
+              <p className="bg-red-100 px-2 py-1 text-red-700 rounded cursor-pointer">
+                Delete
+              </p>
+            </div>
+            {user && user.isDoctor ? (
               ""
             ) : (
-              <div className="flex justify-between mt-4 mb-4" onClick={() => alert("This panel is in Development")}>
-                <p>Download Badge</p>
-                <p className="bg-blue-100 px-2 py-1 text-blue-700 rounded cursor-pointer">Get Now</p>
+              <div
+                className="flex justify-between mt-4 mb-4"
+                onClick={BecomeDoctor}
+              >
+                <p>Become a Doctor</p>
+                <p className="bg-red-100 px-2 py-1 text-green-700 rounded cursor-pointer">
+                  Doctor
+                </p>
               </div>
             )}
-            <div className="flex justify-between mt-4 mb-4" onClick={() => alert("This panel is in Development")}>
-              <p>Get Certificate</p>
-              <p className="bg-blue-100 px-2 py-1 text-blue-700 rounded cursor-pointer">Get Now</p>
-            </div>
-            <div className="flex justify-between mt-4 mb-4" onClick={() => alert("This panel is in Development")}>
-              <p>Reset Password</p>
-              <p className="bg-green-100 px-2 py-1 text-green-700 rounded cursor-pointer">Reset</p>
-            </div>
-            <div className="flex justify-between mt-4 mb-4" onClick={handleEditClick}>
-              <p>Edit Profile</p>
-              <p className="bg-yellow-100 px-2 py-1 text-yellow-700 rounded cursor-pointer">Edit</p>
-            </div>
-            <div className="flex justify-between mt-4 mb-4" onClick={handleDeleteClick}>
-              <p>Delete Account</p>
-              <p className="bg-red-100 px-2 py-1 text-red-700 rounded cursor-pointer">Delete</p>
-            </div>
           </div>
         </div>
       </div>
-  
+
       {isEditing && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
           <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
@@ -307,18 +413,32 @@ function Profile() {
               />
             </div>
             <div className="flex justify-between">
-              <button onClick={handleSaveClick} className="bg-blue-500 text-white px-4 py-2 rounded">Save</button>
-              <button onClick={handleCancelClick} className="bg-gray-500 text-white px-4 py-2 rounded">Cancel</button>
+              <button
+                onClick={handleSaveClick}
+                className="bg-blue-500 text-white px-4 py-2 rounded"
+              >
+                Save
+              </button>
+              <button
+                onClick={handleCancelClick}
+                className="bg-gray-500 text-white px-4 py-2 rounded"
+              >
+                Cancel
+              </button>
             </div>
           </div>
         </div>
       )}
-  
+
       {isDeleting && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
           <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
-            <h2 className="text-2xl font-bold mb-4 text-red-600">Delete Account</h2>
-            <p className="text-gray-600 mb-4">Please enter your password to confirm.</p>
+            <h2 className="text-2xl font-bold mb-4 text-red-600">
+              Delete Account
+            </h2>
+            <p className="text-gray-600 mb-4">
+              Please enter your password to confirm.
+            </p>
             <input
               type="password"
               value={deletePassword}
@@ -326,8 +446,58 @@ function Profile() {
               className="w-full border rounded p-2 mb-4"
             />
             <div className="flex justify-between">
-              <button onClick={handleDeleteAccount} className="bg-red-500 text-white px-4 py-2 rounded">Delete</button>
-              <button onClick={handleDeleteCancelClick} className="bg-gray-500 text-white px-4 py-2 rounded">Cancel</button>
+              <button
+                onClick={handleDeleteAccount}
+                className="bg-red-500 text-white px-4 py-2 rounded"
+              >
+                Delete
+              </button>
+              <button
+                onClick={handleDeleteCancelClick}
+                className="bg-gray-500 text-white px-4 py-2 rounded"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {DoctorForm && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
+            <h2 className="text-2xl font-bold mb-4 text-blue-600">
+              Verify Yourself
+            </h2>
+            <p className="text-gray-600 mb-4">Please Detail to confirm.</p>
+            <input
+              type="text"
+              value={RMP_NO}
+              onChange={(e) => setRMP_NO(e.target.value)}
+              className="w-full border rounded p-2 mb-4"
+              required
+            />
+            <input
+              type="file"
+              name="RMPCertificate"
+              onChange={(e) => setRMP_img(e.target.files[0])}
+              className="w-full border rounded p-2 mb-4"
+              required
+            />
+
+            <div className="flex justify-between">
+              <button
+                onClick={handleReqToBeDoctor}
+                className="bg-blue-500 text-white px-4 py-2 rounded"
+              >
+                Request to Admin
+              </button>
+              <button
+                onClick={handleBecomeDoctorCancelclick}
+                className="bg-gray-500 text-white px-4 py-2 rounded"
+              >
+                Cancel
+              </button>
             </div>
           </div>
         </div>
